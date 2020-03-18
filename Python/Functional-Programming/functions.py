22a97a175939dd9f252aa1cf806b5fe0a2b7bd56
gc126 <gc126@duke.edu>
2020-03-18 18:54:06 -0400

    map+filter+reduce of friends dictionary

--- Python/Functional-Programming/functions.py
+++ Python/Functional-Programming/functions.py
@@ -2,8 +2,46 @@
 from functools import reduce
 num_list = [0, 1, 2, 3, 4, 5, 6, 7]
 def get_sum(acc, element):
-    print(f'acc is {acc}, element is {element}')
     return acc + element
-
 sum = reduce(get_sum, num_list)
-print(sum)
+## Example of combining map, filter, and reduce to find the average gpa of my friends based on where they are going to work
+friends = [{
+    'name': 'Henock',
+    'gpa': 4,
+    'work_location': 'NYC'
+},
+{
+    'name': 'Jack',
+    'gpa': 4,
+    'work_location': 'Boston'
+},
+{
+    'name': 'Evan',
+    'gpa': 3.5,
+    'work_location': 'Durham'
+},
+{
+    'name': 'Quinci',
+    'gpa': 3.3,
+    'work_location': 'Atlanta'
+},
+{
+    'name': 'John',
+    'gpa': 4,
+    'work_location': 'Durham'
+},
+]
+
+def w_location(friend):
+    return friend['work_location'] == 'Durham'
+
+durham = list(filter(w_location, friends)) #filter friends based on working in Durham
+
+def get_gpa(friend):
+    return friend['gpa']
+gpas = list(map(get_gpa, durham)) 
+print(gpas)
+
+total_gpa = reduce(get_sum, gpas)
+average_gpa = total_gpa / len(gpas)
+print(average_gpa)
