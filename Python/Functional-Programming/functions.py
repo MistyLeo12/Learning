9c3755ae5be37835e34f5ee3dd0056b06e726e59
gc126 <gc126@duke.edu>
2020-03-20 13:43:16 -0400

    Leanred partial application in functional programming

--- Python/Functional-Programming/functions.py
+++ Python/Functional-Programming/functions.py
@@ -45,3 +45,28 @@ print(gpas)
 total_gpa = reduce(get_sum, gpas)
 average_gpa = total_gpa / len(gpas)
 print(average_gpa)
+
+# Partial Application:
+# Takes a function and fixes some of the arguements beforehand
+def add(x, y, z):
+    return x + y + z
+
+def add_partial(x):
+    def add_others(y, z):
+        return x + y + z
+    return add_others
+
+add_10 = add_partial(10)
+print(add_10(4,7))
+
+def curry_add(x):
+    def curry_add_inner(y):
+        def curry_add_inner2(z):
+            return x + y +z
+        return curry_add_inner2
+    return curry_add_inner
+
+add_10 = curry_add(10)
+add_10_and_8 = add_10(8)
+#print(add_10_and_8(2))
+#print(curry_add(10)(8)(2))
